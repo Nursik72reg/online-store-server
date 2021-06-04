b24128bf5e99edd4fa5c5e00074e3fc24d59eece
Nursultan Tashanov <tashanov.98@mail.ru>
2021-06-04 18:55:44 +0500

    '..'

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const {Sequelize} = require('sequelize')
 module.exports = new Sequelize(
     process.env.DB_NAME, // Название БД
     process.env.DB_USER, // Пользователь
-    process.env.DB_PASSWORD, // ПАРОЛЬ
+  /*  process.env.DB_PASSWORD, // ПАРОЛЬ*/
     {
         dialect: 'postgres',
         host: process.env.DB_HOST,
